b28155019e5faaaccbfc11c43d28929a8e3ba802
Louis Melahn <lmelahn@legionaries.org>
2017-04-19 21:38:42 +0200

    Initial Commit

---
+++ PiEstimator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PiEstimator
+//
+//  Created by Louis Melahn on 4/19/17.
+//  Copyright © 2017 Louis Melahn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
